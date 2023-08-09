a5dd147d8d6e4b6e0786bd16ff2b579c273689df
Adam Fine <afine@Adams-MacBook-Pro.local>
2023-08-08 21:55:38 -0400

    styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginScreenComponent } from './login-screen/login-screen.component';
+import { RegisterComponent } from './register/register.component';
+import { ReturningCustomerloginComponent } from './returning-customerlogin/returning-customerlogin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginScreenComponent,
+    RegisterComponent,
+    ReturningCustomerloginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/returning-customerlogin/returning-customerlogin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-returning-customerlogin',
+  templateUrl: './returning-customerlogin.component.html',
+  styleUrls: ['./returning-customerlogin.component.scss']
+})
+export class ReturningCustomerloginComponent {
+
+}
